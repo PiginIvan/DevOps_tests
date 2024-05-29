e0d0e9c898787176a9be7d028ea803ab85a9bc5c
PiginIvan <piginivan4@gmail.com>
2024-05-29 23:45:46 +0300

    add functions

---
+++ src/main.py
@@ -0,0 +1,11 @@
+def fibonacci(n):
+    if n in (1, 2):
+        return 1
+    return fibonacci(n - 1) + fibonacci(n - 2)
+
+def sum_of_digits(num):
+    sum = 0
+    while num > 0:
+        sum += num % 10
+        num //= 10
+    return sum
