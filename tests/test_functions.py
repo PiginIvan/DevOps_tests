35dfe8425fe0c9640a635d9725bbae790c745261
PiginIvan <piginivan4@gmail.com>
2024-05-29 23:48:36 +0300

    add tests

---
+++ tests/test_functions.py
@@ -0,0 +1,24 @@
+import pytest
+from src.main import *
+
+@pytest.mark.parametrize(
+    ('n', 'ans'), [
+        (3, 2),
+        (6, 8),
+        (10, 55),
+    ]
+)
+
+def test_function_fibonacci(n, ans):
+    assert fibonacci(n) == ans
+
+@pytest.mark.parametrize(
+    ('num', 'sum'), [
+        (1157, 14),
+        (33, 6),
+        (100, 1),
+    ]
+)
+
+def test_function_sum_of_digits(num, sum):
+    assert sum_of_digits(num) == sum
